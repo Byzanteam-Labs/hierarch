3993069834a5105f51b7268422c3b3b78a776869
Phil Chen <06fahchen@gmail.com>
2018-06-28 23:12:15 +0800

    Create EXTENSION IF NOT EXISTS

--- priv/repo/migrations/20180625095801_create_catelogs.exs
+++ priv/repo/migrations/20180625095801_create_catelogs.exs
@@ -2,7 +2,7 @@ defmodule Dummy.Repo.Migrations.CreateCatelogs do
   use Ecto.Migration
 
   def change do
-    execute "CREATE EXTENSION ltree" # Enables Ltree action
+    execute "CREATE EXTENSION IF NOT EXISTS ltree" # Enables Ltree action
 
     create table(:catelogs, primary_key: false) do
       add :id, :uuid, primary_key: true
