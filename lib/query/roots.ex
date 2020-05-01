c807a432dd7ec973570fe676f35ff12fce36bf13
Phil Chen <06fahchen@gmail.com>
2020-05-01 22:52:13 +0800

    refactor: roots_condition

--- lib/query/roots.ex
+++ lib/query/roots.ex
@@ -1,15 +1,20 @@
 defmodule Hierarch.Query.Roots do
   import Ecto.Query
 
+  @roots_path ""
+
   @doc """
   Return query expressions for roots
   """
   def query(schema) do
-    roots_path = ""
+    condition= roots_condition(schema)
 
-    from(
-      t in schema,
-      where: field(t, ^schema.__hierarch__(:path_column)) == ^roots_path
-    )
+    from schema, where: ^condition
+  end
+
+  def roots_condition(schema) do
+    path_column = schema.__hierarch__(:path_column)
+
+    dynamic([q], field(q, ^path_column) == ^@roots_path)
   end
 end
