96a3fc9c6c8b9b44c627111c2d57e40036d1d84e
Phil Chen <06fahchen@gmail.com>
2020-05-01 23:15:11 +0800

    refactor siblings conditions

--- lib/query/siblings.ex
+++ lib/query/siblings.ex
@@ -5,28 +5,27 @@ defmodule Hierarch.Query.Siblings do
   Return query expressions for siblings
   ## Options
 
-    * `:with_self` - when true to include itself. Defaults to false.
+    * `:with_self` - when true to include itself. Default is false.
   """
   def query(%schema{} = struct, opts \\ []) do
-    with_self = Keyword.get(opts, :with_self, false)
+    condition = siblings_with_self_condition(struct)
 
+    if Keyword.get(opts, :with_self, false) do
+      from schema, where: ^condition
+    else
+      from schema, where: ^condition, where: ^excluding_self_condition(struct)
+    end
+  end
+
+  defp siblings_with_self_condition(%schema{} = struct) do
+    parent_path = Hierarch.Util.struct_path(struct)
+
+    dynamic([q], field(q, ^schema.__hierarch__(:path_column)) == ^parent_path)
+  end
+
+  defp excluding_self_condition(%_schema{} = struct) do
     [{pk_column, value}] = Ecto.primary_key(struct)
 
-    path = Hierarch.Util.struct_path(struct)
-    parent_path = path
-
-    siblings_with_self_query =
-      from(
-        t in schema,
-        where: field(t, ^schema.__hierarch__(:path_column)) == ^parent_path
-      )
-
-    case with_self do
-      true ->
-        siblings_with_self_query
-
-      _ ->
-        siblings_with_self_query |> where([t], field(t, ^pk_column) != ^value)
-    end
+    dynamic([q], field(q, ^pk_column) != ^value)
   end
 end
